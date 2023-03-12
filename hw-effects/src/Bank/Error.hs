32e2d0b1a120f0ee9a603ef2341bd0d2d67f63db
Semyon Stepanov <opopopa705@gmail.com>
2023-03-12 21:28:11 +0300

    errors

--- hw-effects/src/Bank/Error.hs
+++ hw-effects/src/Bank/Error.hs
@@ -3,6 +3,19 @@ module Bank.Error where
 import Control.Exception (Exception)
 
 -- (1 балл) Перечислите всё, что может пойти не так в ходе выполнения операций
+-- newAccount
+--  m a is Nothing
+-- balance
+--  m b is Nothing
+--  b < 0
+-- deposit
+--  итоговый баланс a < 0
+--  m () is Nothing
+-- withdraw
+--  итоговый баланс a < 0
+--  m () is Nothing
+-- deleteAccount
+--  m () is Nothing
 -- из @MonadBank@.
 
 data BankError = DummyError deriving (Show)
